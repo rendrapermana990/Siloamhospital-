7dfe01cc1817155c3c4fe6b673c5d1ab0b38208d
rendrapermana990 <61962542+rendrapermana990@users.noreply.github.com>
2024-03-28 16:44:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using DotNet8API.services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSingleton<IOurHeroService, OurHeroService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
