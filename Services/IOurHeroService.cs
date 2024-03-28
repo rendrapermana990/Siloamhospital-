6fe88899e3110d0857c04a1be436048f1253c4b8
rendrapermana990 <61962542+rendrapermana990@users.noreply.github.com>
2024-03-28 16:47:46 +0700

    Add files via upload

---
+++ Services/IOurHeroService.cs
@@ -0,0 +1,17 @@
+﻿using DotNet8API.Model;
+
+namespace DotNet8API.services
+{
+    public interface IOurHeroService
+    {
+        List<OurHero> GetAllHeros(bool? isActive);
+
+        OurHero? GetHerosByID(int id);
+
+        OurHero AddOurHero(AddUpdateOurHero obj);
+
+        OurHero? UpdateOurHero(int id, AddUpdateOurHero obj);
+
+        bool DeleteHerosByID(int id);
+    }
+}
