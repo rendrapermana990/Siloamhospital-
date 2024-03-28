ed9c845ca78f5afa567d19857c42fdb886ed3a3d
rendrapermana990 <61962542+rendrapermana990@users.noreply.github.com>
2024-03-28 16:50:23 +0700

    Add files via upload

---
+++ Model/OurHero.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DotNet8API.Model
+{
+    public class OurHero
+    {
+        public int Id { get; set; } = 0;
+        public required string FirstName { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public bool isActive { get; set; } = true;
+    }
+}
